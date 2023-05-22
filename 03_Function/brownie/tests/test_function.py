e35c9a7b82ca8bd41b44801cb376191189140f40
lightnlightning <lightning004@outlook.com>
2023-05-23 06:56:12 +0800

    部署合约、测试初始化变量

---
+++ 03_Function/brownie/tests/test_function.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import pytest
+
+@pytest.fixture(scope="function", autouse=True)
+def isolate(fn_isolation):
+    # perform a chain rewind after completing each test, to ensure proper isolation
+    # https://eth-brownie.readthedocs.io/en/v1.10.3/tests-pytest-intro.html#isolation-fixtures
+    pass
+
+
+@pytest.fixture(scope="module")
+def functiontypes(FunctionTypes, accounts):
+    return FunctionTypes.deploy( {'from': accounts[0],'value':89})
+
+def test_init(functiontypes,accounts):
+    assert functiontypes.balance() == 89 
+    assert functiontypes.number() == 5 
